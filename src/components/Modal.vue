807e59acd0d7919a3ef49fe900f4dea3b1bb7ed3
flyinacres <ron-github@rfgf.com>
2023-01-10 17:29:01 -0800

    emit

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,16 +2,46 @@
     <teleport to=".modals-container">
         <div 
             class="modal">
-            <h1>
-                <slot name="title" />
+            <h1>{{ title }}
             </h1>
             <slot />
-            <button>Hide modal</button>
+            <button @click="handleButtonClick">Hide modal</button>
 
         </div>
     </teleport>
 </template>
 
+
+<script setup>
+    // For some reason I don't have to import this in vue.
+    const props = defineProps({
+    title: {
+        type: String,
+        default: 'No title specified'
+    }
+    })
+
+    const emit = defineEmits(['hideModal'])
+
+    const handleButtonClick = () => {
+        emit('hideModal')
+    }
+
+/* If I want to access the props programmatically I would need to use props.title, not just title */
+</script>
+
+<!--
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                default: 'No title specified'
+            }
+        }
+    }
+</script>
+-->
 
 <style>
 .modal {
--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,8 +2,10 @@
     <div class="modals">
         <h2>Modals</h2>
         <button @click="showModal = true">Show Modal</button>
-        <Modal v-if="showModal">
-            <template v-slot:title>My New Title</template>
+        <Modal v-if="showModal"
+            @hideModal="showModal=false"
+            title="My modal title (via prop)">
+            
             <p>Arliquip sit ea excepteur ex incididunt. Esse sunt aliquip dolor minim do id tempor velit. Nulla nostrud sit qui proident magna labore sunt dolor magna reprehenderit nostrud do. Laboris do nostrud anim nulla Lorem amet elit laborum laboris. Anim sit nisi sint qui sint. Ullamco id veniam do ea quis deserunt. Labore irure quis nulla occaecat excepteur sit.
             </p>
         </Modal>
